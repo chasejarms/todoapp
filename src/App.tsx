1920dde706c59ea9982c35dfcffb26eb75cfa8e2
Chase Armstrong <I21664@xactware.com>
2019-08-05 12:35:48 -0600

    Add in logs for lifecycle hooks

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { cloneDeep } from 'lodash';
 import { Todo } from './Todo';
 
 export interface ITodo {
+  id: string;
   name: string;
   complete: boolean;
 }
@@ -13,15 +14,71 @@ export interface IAppProps {}
 export interface IAppState {
   todos: ITodo[];
   todoName: string;
+  todosThatAreBeingDeleted: {
+    [id: string]: boolean;
+  };
+  loadingTodos: boolean;
 }
 
 class App extends React.Component<IAppProps, IAppState> {
   public state: IAppState = {
     todos: [],
     todoName: '',
+    todosThatAreBeingDeleted: {},
+    loadingTodos: true,
   };
 
+  public async componentWillMount(): Promise<void> {
+    console.log('App: WillMount');
+    try {
+      const todos = await getTodos();
+      this.setState({
+        todos,
+        loadingTodos: false,
+      });
+    } catch {
+      this.setState({
+        loadingTodos: false,
+      })
+    }
+  }
+
+  public componentDidMount(): void {
+    console.log('App: DidMount');
+  }
+
+  public componentWillReceiveProps(): void {
+    console.log('App: WillReceiveProps');
+  }
+
+  public shouldComponentUpdate(nextProps: IAppProps, nextState: IAppState, nextContext: any): boolean {
+    console.log('App: ShouldUpdate');
+    return true;
+  }
+
+  public componentWillUpdate(): void {
+    console.log('App: WillUpdate');
+  }
+
+  public componentDidUpdate(): void {
+    console.log('App: DidUpdate');
+  }
+
+  public componentWillUnmount(): void {
+    console.log('App: WillUnmount');
+  }
+
   public render() {
+    console.log('App: Render');
+
+    if (this.state.loadingTodos) {
+      return (
+        <div>
+          <p>Loading...</p>
+        </div>
+      )
+    }
+
     const todos = this.state.todos.map((todo, index) => {
       return (
         <Todo
@@ -29,6 +86,7 @@ class App extends React.Component<IAppProps, IAppState> {
           todo={todo}
           onClickCheckbox={this.handleCheckboxTodoChange(index)}
           onClickDeleteButton={this.deleteTodo(index)}
+          isDeleting={this.state.todosThatAreBeingDeleted[todo.id]}
         />
       )
     });
@@ -42,6 +100,9 @@ class App extends React.Component<IAppProps, IAppState> {
           </div>
           <button type="submit" onClick={this.onAddTodoClick} disabled={this.state.todoName === ''} className={'btn btn-primary'}>Add Todo</button>
         </form>
+        {/* <div className="delete-all-todos-container">
+          <button type="button" onClick={this.deleteAllTodos} disabled={this.state.todos.length < 0} className={'btn btn-danger'}>Delete All Todos</button>
+        </div> */}
         {todos.length === 0 ? (
           <div className="no-todos-container">
             <p>You have not added any todos</p>
@@ -76,8 +137,13 @@ class App extends React.Component<IAppProps, IAppState> {
 
   public deleteTodo = (index: number) => () => {
     const clonedTodos = cloneDeep(this.state.todos);
+    const clonedTodosThatAreBeingDeleted = cloneDeep(this.state.todosThatAreBeingDeleted);
+    const todoId = this.state.todos[index].id;
+    clonedTodosThatAreBeingDeleted[todoId] = true;
     clonedTodos.splice(index, 1);
+
     this.setState({
+      todosThatAreBeingDeleted: clonedTodosThatAreBeingDeleted,
       todos: clonedTodos,
     })
   }
@@ -86,6 +152,7 @@ class App extends React.Component<IAppProps, IAppState> {
     event.preventDefault();
 
     const updatedTodos = [...this.state.todos, {
+      id: generateUniqueId(),
       name: this.state.todoName,
       complete: false,
     }];
@@ -98,3 +165,32 @@ class App extends React.Component<IAppProps, IAppState> {
 }
 
 export default App;
+
+function getTodos(): Promise<ITodo[]> {
+  throw new Error();
+  // return new Promise((resolve) => {
+  //   setTimeout(() => {
+  //     resolve([
+  //       {
+  //         id: generateUniqueId(),
+  //         name: 'Laundry',
+  //         complete: false,
+  //       },
+  //       {
+  //         id: generateUniqueId(),
+  //         name: 'Groceries',
+  //         complete: false,
+  //       },
+  //       {
+  //         id: generateUniqueId(),
+  //         name: 'Soccer Practice',
+  //         complete: false,
+  //       },
+  //     ])
+  //   }, 3000);
+  // });
+}
+
+function generateUniqueId(): string {
+  return `${Date.now()}_${Math.random().toString(36).substr(2, 9)}`;
+}
--- src/Todo.tsx
+++ src/Todo.tsx
@@ -5,11 +5,42 @@ export interface ITodoProps {
     todo: ITodo;
     onClickCheckbox: (event: any) => void;
     onClickDeleteButton: () => void;
+    isDeleting?: boolean;
 }
 export interface ITodoState {}
 
 export class Todo extends React.Component<ITodoProps, ITodoState> {
+    public componentWillMount(): void {
+        console.log('Todo: WillMount');
+    }
+
+    public componentDidMount(): void {
+        console.log('Todo: DidMount');
+    }
+
+    public componentWillReceiveProps(): void {
+        console.log('Todo: WillReceiveProps');
+    }
+
+    public shouldComponentUpdate(nextProps: ITodoProps, nextState: ITodoState, nextContext: any): boolean {
+        console.log('Todo: ShouldUpdate');
+        return true;
+    }
+
+    public componentWillUpdate(): void {
+        console.log('Todo: WillUpdate');
+    }
+
+    public componentDidUpdate(): void {
+        console.log('Todo: DidUpdate');
+    }
+
+    public componentWillUnmount(): void {
+        console.log('Todo: WillUnmount');
+    }
+
     public render() {
+        console.log('Todo: Render');
         const todo = this.props.todo;
 
         return (
@@ -20,7 +51,7 @@ export class Todo extends React.Component<ITodoProps, ITodoState> {
                     <label className="form-check-label">Complete</label>
                 </div>
                 <div className="delete-todo-button-container">
-                    <button className="btn btn-danger" onClick={this.props.onClickDeleteButton}>Delete</button>
+                    <button className="btn btn-danger" onClick={this.props.onClickDeleteButton} disabled={this.props.isDeleting}>Delete</button>
                 </div>
             </div>
         )
