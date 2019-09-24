cebbfddc6aee97a4a63d67d27894b0b3f67a04bc
Chase Armstrong <I21664@xactware.com>
2019-09-24 11:14:51 -0600

    Refactor as hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import { cloneDeep } from 'lodash';
 import { Todo } from './Todo';
+import { api } from '../src/Api';
 
 export interface ITodo {
   name: string;
@@ -15,86 +16,178 @@ export interface IAppState {
   todoName: string;
 }
 
-class App extends React.Component<IAppProps, IAppState> {
-  public state: IAppState = {
-    todos: [],
-    todoName: '',
-  };
+export function App(props: IAppProps) {
+  const [todos, setTodos] = useState<ITodo[]>([]);
+  const [todoName, setTodoName] = useState('');
+  useEffect(() => {
+    api.getTodos().then((todosFromServer) => {
+      setTodos(todosFromServer);
+    })
+  }, []);
 
-  public render() {
-    const todos = this.state.todos.map((todo, index) => {
-      return (
-        <Todo
-          key={index}
-          todo={todo}
-          onClickCheckbox={this.handleCheckboxTodoChange(index)}
-          onClickDeleteButton={this.deleteTodo(index)}
-        />
-      )
-    });
+  useEffect(() => {
+    api.updateTodos(todos);
+  }, [todos]);
 
-    return (
-      <div className="app-container">
-        <form>
-          <div className="form-group">
-            <label >Todo Name</label>
-            <input value={this.state.todoName} onChange={this.handleTodoChange} className="form-control"/>
-          </div>
-          <button type="submit" onClick={this.onAddTodoClick} disabled={this.state.todoName === ''} className={'btn btn-primary'}>Add Todo</button>
-        </form>
-        {todos.length === 0 ? (
-          <div className="no-todos-container">
-            <p>You have not added any todos</p>
-          </div>
-        ) : (
-          <div className="todos-container">
-            <ul className="todos-ul">
-              {todos}
-            </ul>
-          </div>
-        )}
-      </div>
-    );
-  }
+  useEffect(() => {
+    const something = setTimeout(() => {
+      console.log('set timeout called');
+    }, 1000);
+    return () => {
+      clearTimeout(something);
+    }
+  })
 
-  public handleTodoChange = (event: any) => {
+  function handleTodoChange(event: any) {
     const newValue = event.target.value;
     // this.state.todoName = newValue;
-    this.setState({
-      todoName: newValue,
-    });
+    setTodoName(newValue);
   }
 
-  public handleCheckboxTodoChange = (index: number) => (event: any) => {
-    const newValue = event.target.checked;
-    const clonedTodos = cloneDeep(this.state.todos);
-    clonedTodos[index].complete = newValue;
-    this.setState({
-      todos: clonedTodos,
-    })
+  function handleCheckboxTodoChange(index: number) {
+    return (event: any) => {
+      const newValue = event.target.checked;
+      const clonedTodos = cloneDeep(todos);
+      clonedTodos[index].complete = newValue;
+      setTodos(clonedTodos);
+    }
   }
 
-  public deleteTodo = (index: number) => () => {
-    const clonedTodos = cloneDeep(this.state.todos);
-    clonedTodos.splice(index, 1);
-    this.setState({
-      todos: clonedTodos,
-    })
+  function deleteTodo(index: number) {
+    return () => {
+      const clonedTodos = cloneDeep(todos);
+      clonedTodos.splice(index, 1);
+      setTodos(clonedTodos);
+    }
   }
 
-  public onAddTodoClick = (event: any) => {
+  function onAddTodoClick(event: any) {
     event.preventDefault();
 
-    const updatedTodos = [...this.state.todos, {
-      name: this.state.todoName,
+    const updatedTodos = [...todos, {
+      name: todoName,
       complete: false,
     }];
 
-    this.setState({
-      todos: updatedTodos,
-      todoName: '',
-    });
+    setTodos(updatedTodos);
+    setTodoName('');
   }
+
+  const todosList = todos.map((todo, index) => {
+    return (
+      <Todo
+        key={index}
+        todo={todo}
+        onClickCheckbox={handleCheckboxTodoChange(index)}
+        onClickDeleteButton={deleteTodo(index)}
+      />
+    )
+  });
+
+  return (
+    <div className="app-container">
+      <form>
+        <div className="form-group">
+          <label >Todo Name</label>
+          <input value={todoName} onChange={handleTodoChange} className="form-control"/>
+        </div>
+        <button type="submit" onClick={onAddTodoClick} disabled={todoName === ''} className={'btn btn-primary'}>Add Todo</button>
+      </form>
+      {todosList.length === 0 ? (
+        <div className="no-todos-container">
+          <p>You have not added any todos</p>
+        </div>
+      ) : (
+        <div className="todos-container">
+          <ul className="todos-ul">
+            {todosList}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
 }
+
+// class App extends React.Component<IAppProps, IAppState> {
+//   public state: IAppState = {
+//     todos: [],
+//     todoName: '',
+//   };
+
+//   public render() {
+//     const todos = this.state.todos.map((todo, index) => {
+//       return (
+//         <Todo
+//           key={index}
+//           todo={todo}
+//           onClickCheckbox={this.handleCheckboxTodoChange(index)}
+//           onClickDeleteButton={this.deleteTodo(index)}
+//         />
+//       )
+//     });
+
+//     return (
+//       <div className="app-container">
+//         <form>
+//           <div className="form-group">
+//             <label >Todo Name</label>
+//             <input value={this.state.todoName} onChange={this.handleTodoChange} className="form-control"/>
+//           </div>
+//           <button type="submit" onClick={this.onAddTodoClick} disabled={this.state.todoName === ''} className={'btn btn-primary'}>Add Todo</button>
+//         </form>
+//         {todos.length === 0 ? (
+//           <div className="no-todos-container">
+//             <p>You have not added any todos</p>
+//           </div>
+//         ) : (
+//           <div className="todos-container">
+//             <ul className="todos-ul">
+//               {todos}
+//             </ul>
+//           </div>
+//         )}
+//       </div>
+//     );
+//   }
+
+//   public handleTodoChange = (event: any) => {
+//     const newValue = event.target.value;
+//     // this.state.todoName = newValue;
+//     this.setState({
+//       todoName: newValue,
+//     });
+//   }
+
+//   public handleCheckboxTodoChange = (index: number) => (event: any) => {
+//     const newValue = event.target.checked;
+//     const clonedTodos = cloneDeep(this.state.todos);
+//     clonedTodos[index].complete = newValue;
+//     this.setState({
+//       todos: clonedTodos,
+//     })
+//   }
+
+//   public deleteTodo = (index: number) => () => {
+//     const clonedTodos = cloneDeep(this.state.todos);
+//     clonedTodos.splice(index, 1);
+//     this.setState({
+//       todos: clonedTodos,
+//     })
+//   }
+
+//   public onAddTodoClick = (event: any) => {
+//     event.preventDefault();
+
+//     const updatedTodos = [...this.state.todos, {
+//       name: this.state.todoName,
+//       complete: false,
+//     }];
+
+//     this.setState({
+//       todos: updatedTodos,
+//       todoName: '',
+//     });
+//   }
+// }
 
 export default App;
